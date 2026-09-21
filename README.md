523758fa1fb483ff3387dafdfe5bcd333e4de7a6
Stefan Boos <kontakt@boos.systems>
2026-09-21 16:55:19 +0200

    test: failing first test

---
+++ README.md
@@ -0,0 +1,7 @@
+# Roman Numerals to Integer
+
+```shell
+python3 -m venv .venv && . .venv/bin/activate
+python test test_convert.py
+```
+
---
+++ test_convert.py
@@ -0,0 +1,9 @@
+import unittest
+
+class TestConvert(unittest.TestCase):
+    def test_i(self):
+        self.assertEqual("actual", "expected")
+
+if __name__ == "__main__":
+    unittest.main()
+
