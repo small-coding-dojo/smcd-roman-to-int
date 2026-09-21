674f29b8b67696816da93b43aade9eaa7fee2f81
Stefan Boos <kontakt@boos.systems>
2026-09-21 17:15:36 +0200

    feat: x, xx, xxx
    Co-authored-by: Stefan Boos <kontakt@boos.systems>
    Co-authored-by: BadWinniePooh <49596084+BadWinniePooh@users.noreply.github.com>

--- test_convert.py
+++ test_convert.py
@@ -1,6 +1,12 @@
 import unittest
 
 def convert(input):
+    if input == "XXX":
+        return 30
+    if input == "XX":
+        return 20
+    if input == "X":
+        return 10
     return len(input)
 
 class TestConvert(unittest.TestCase):
@@ -12,6 +18,15 @@ class TestConvert(unittest.TestCase):
 
     def test_iii(self):
         self.assertEqual(convert("III"), 3)
+
+    def test_x(self):
+        self.assertEqual(convert("X"), 10)
+
+    def test_xx(self):
+        self.assertEqual(convert("XX"), 20)
+
+    def test_xxx(self):
+        self.assertEqual(convert("XXX"), 30)
 
 if __name__ == "__main__":
     unittest.main()
