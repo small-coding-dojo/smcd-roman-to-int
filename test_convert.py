a62a1b621ef2d1572caab945e2b7131a9150f2aa
Niklas Rueber <nrueber@igus.net>
2026-09-21 17:06:05 +0200

    feat: we can convert I, II and III reliably
    Co-authored-by: Stefan Boos <kontakt@boos.systems>
    Co-authored-by: BadWinniePooh <49596084+BadWinniePooh@users.noreply.github.com>

--- test_convert.py
+++ test_convert.py
@@ -1,8 +1,21 @@
 import unittest
 
+def convert(input):
+    if input == "III":
+        return 3
+    if input == "II":
+        return 2
+    return 1
+
 class TestConvert(unittest.TestCase):
     def test_i(self):
-        self.assertEqual("actual", "expected")
+        self.assertEqual(convert("I"), 1)
+
+    def test_ii(self):
+        self.assertEqual(convert("II"), 2)
+
+    def test_iii(self):
+        self.assertEqual(convert("III"), 3)
 
 if __name__ == "__main__":
     unittest.main()
